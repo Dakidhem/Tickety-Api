b825d3e18bff713904b01ce94840f76f8fc7cb5d
[MehdiSfizer] <[mehdi@sfizer.consulting]>
2023-12-05 01:29:50 +0100

    media

--- TicketingApi/urls.py
+++ TicketingApi/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('api/tickets/',include('tickets.urls')),
     path('api/notifications/',include('notifications.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
+urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
